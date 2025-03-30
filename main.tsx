e0d56035c264c435b2be957e298c16db1d072a3f
jderochervlk <josh@vlkpack.com>
2025-03-29 22:22:12 -0400

    basics are here

--- main.tsx
+++ main.tsx
@@ -1,26 +1,61 @@
 import satori from "npm:satori"
 import { html } from "npm:satori-html"
 
-const robotoArrayBuffer = await Deno.readFile("./Roboto-Regular.ttf")
-const svg = await satori(
-  html(`<div style="background: red; padding: 25px;">Hello!</div>`),
-  {
-    width: 600,
-    height: 400,
-    fonts: [
-      {
-        name: "Roboto",
-        data: robotoArrayBuffer,
-        weight: 400,
-        style: "normal",
-      },
-    ],
-  },
-)
+type Args = {
+  title: string
+  tag: string
+  date: string
+  author: string
+  img: string
+}
 
-Deno.serve((res) => {
+async function makeImg({ title, tag, date, author, img }: Args) {
+  const robotoArrayBuffer = await Deno.readFile("./Roboto-Regular.ttf")
+  const svg = await satori(
+    html`
+    <div style="
+        background: rgb(255, 255, 255);
+        color: rgb(35, 37, 56); 
+        width: 100%;
+        height: 100%;
+        display:flex
+      ">
+        <h1>${title}</h2>
+        <p>${tag}</h2>
+        <p>${author}</p>
+        <p>${date}</p>
+        <img src=${img}/>
+      </div>
+    `,
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "Roboto",
+          data: robotoArrayBuffer,
+          weight: 400,
+          style: "normal",
+        },
+      ],
+    },
+  )
+  return svg
+}
+const test =
+  "?title=ReScript%20Retreat&tag=Accelerating%20ReScript%20development%20through%20meeting%20in-person.&date=Mar%2017%2C%202025&author=ReScript%20Association&img=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1045362176117100545%2FMioTQoTp_400x400.jpg"
+
+Deno.serve(async (res) => {
   const url = new URL(res.url)
-  const searchParams = new URLSearchParams(res.url)
+  const searchParams = new URLSearchParams(url.searchParams)
+
+  const title = searchParams.get("title") ?? ""
+  const tag = searchParams.get("tag") ?? ""
+  const date = searchParams.get("date") ?? ""
+  const author = searchParams.get("author") ?? ""
+  const img = searchParams.get("img") ?? ""
+
+  const svg = await makeImg({ title, tag, date, author, img })
   switch (url.pathname) {
     case "/":
       return new Response(svg, { headers: { "Content-Type": "image/svg+xml" } })
@@ -28,15 +63,15 @@ Deno.serve((res) => {
       return new Response()
     case "/test":
       return new Response(
-        `<html prefix="og: https://ogp.me/ns#">
-        <head>
-        <title>The Rock (1996)</title>
-        <meta property="og:title" content="The Rock" />
-        <meta property="og:type" content="video.movie" />
-        <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
-        <meta property="og:image" content="https://joshderoche-deno-og-ima-93-4fxpc5cs4p2p.deno.dev/" />
-        </head>
-        <div>Test</div>
+        `
+        <html prefix="og: https://ogp.me/ns#">
+          <head>
+          <title>OG test page</title>
+          <meta property="og:title" content="Open Graph Test Page" />
+          <meta property="og:url" content="https://joshderoche-deno-og-ima-93-4fxpc5cs4p2p.deno.dev/test" />
+          <meta property="og:image" content="https://joshderoche-deno-og-ima-93-4fxpc5cs4p2p.deno.dev?title=ReScript%20Retreat&tag=Accelerating%20ReScript%20development%20through%20meeting%20in-person.&date=Mar%2017%2C%202025&author=ReScript%20Association&img=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1045362176117100545%2FMioTQoTp_400x400.jpg" />
+          </head>
+          <div>Test</div>
         </html>
         `,
         { headers: { "Content-Type": "text/html" } },
